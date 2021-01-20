2a84064ea73465fd3f37ade46952604b3e34ed3f
GitHub Action <action@github.com>
2021-01-20 08:38:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/apcx/json2dio/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/apcx/json2dio/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.apcx.json2dio.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.apcx.json2dio.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/apcx/json2dio/services/MyProjectService.kt
+++ src/main/kotlin/com/github/apcx/json2dio/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.apcx.json2dio.services
 
+import com.github.apcx.json2dio.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
